027b846659290112541596299422f0834d405159
yanmifeakeju <yanmifeakeju@gmail.com>
2020-05-05 09:08:18 +0100

    First commit in CustomCMS

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="mt-5 p-3 text-center bg-light">
+    CustomCMS &copy;
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require 'includes/header.php'?>
+
+  <div class="container postsContainer">
+    <div id="posts">
+    <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="card-title">First Post</h4>
+            <p class="card-text">Post body</p>
+            <a href="#" class="edit card-link" data-id="id">
+              <i class="fa fa-pencil"></i>
+            </a>
+
+            <a href="#" class="delete card-link" data-id="id">
+            <i class="fa fa-remove"></i>
+          </a>
+          </div>
+        </div>
+    </div>
+  </div>
+  <?php require 'includes/footer.php'?>
